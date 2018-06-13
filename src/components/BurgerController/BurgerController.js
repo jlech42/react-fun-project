843cb211d29e69cdcd62b8fd2495200760243758
Justin Lechner <jlech42@gmail.com>
2018-06-13 14:49:02 -0400

    Container buildout

---
+++ src/components/BurgerController/BurgerController.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+const burgerController = (props) => <div>Controller</div>
+
+export default burgerController;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
+import BurgerController from '../../components/BurgerController/BurgerController';
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    }
+  }
   render() {
     return(
       <Aux>
-        <div> Burger </div>
-        <div> Controllers </div>
+        <Burger ingredients={this.state.ingredients}/>
+        <BurgerController />
       </Aux>
     );
   }
